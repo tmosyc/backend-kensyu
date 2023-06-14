6a86d245d1acaddc14b61ac30d8a8eae35d8995d
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-06-14 18:04:44 +0900

    formが空欄の時、エラーメッセージを表示する

--- src/app/Handler/PostArticleHandler.php
+++ src/app/Handler/PostArticleHandler.php
@@ -18,9 +18,10 @@ class PostArticleHandler implements HandlerInterface
      */
     public function run(array $req): array
     {
-        $article = new Article(title: $_POST['title'],content: $_POST['content']);
-        PostArticleService::ArticlePostList($article);
-
+        if (strlen($_POST['title']) >= 1 && strlen($_POST['content']) >= 1) {
+            $article = new Article(title: $_POST['title'], content: $_POST['content']);
+            PostArticleService::ArticlePostList($article);
+        }
         $result = self::render(PostService::getPostList());
 
         return [
@@ -35,6 +36,10 @@ class PostArticleHandler implements HandlerInterface
      */
     private static function render(array $posts): string
     {
+        $validation_text="";
+        if (empty($_POST['title']) || empty($_POST['content'])){
+            $validation_text = '文字を入れてください';
+        }
         $body = "<body>";
         $body .= "<h1>記事一覧</h1>";
         $body .= "<form action='/posts' method='post'>";
@@ -42,6 +47,7 @@ class PostArticleHandler implements HandlerInterface
         $body .= "<input type='text' name='content' size=30 placeholder='内容を入力してください'> ";
         $body .= "<button type='submit' name='content_post'>submit</button> ";
         $body .= "</form>";
+        $body .= "<p><font color='red'>{$validation_text}</font></p>";
         foreach ($posts as $post) {
             $title = htmlspecialchars($post->title);
             $body .= "<a href=posts/$post->id>$title</a>";
