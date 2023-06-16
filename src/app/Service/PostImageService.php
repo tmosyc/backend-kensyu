281f73e6cc90b731a0837531b2c71bd9c128f07f
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-06-16 12:24:09 +0900

    画像uploadの form

--- src/app/Service/PostImageService.php
+++ src/app/Service/PostImageService.php
@@ -1,1 +1,15 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Repository\PostImageRepository;
+
+class PostImageService
+{
+    public static function PostImageList($image_array)
+    {
+        return PostImageRepository::postInsertImage($image_array);
+    }
+}
