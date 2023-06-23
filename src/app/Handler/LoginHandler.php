200a3a6824208f69b34d325c872e3fe3f8a26271
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-06-23 17:46:48 +0900

    ログインハンドラーとログアウトハンドラーを作成しました

--- src/app/Handler/LoginHandler.php
+++ src/app/Handler/LoginHandler.php
@@ -1,1 +1,31 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+class LoginHandler implements HandlerInterface
+{
+    public function run(array $req): array
+    {
+        $result = self::render();
+
+        return [
+            'status_code' => 200,
+            'body' => "<html>{$result}</html>",
+        ];
+    }
+
+    public static function render()
+    {
+        $body = "<body>";
+        $body .= "<form action='/post' method='post'>";
+        $body .= "<input type='email' name='email' placeholder='メールアドレス'>";
+        $body .= "<input type='text' name='password' placeholder='パスワード'>";
+        $body .= "<button type='submit' name='register'>Sign Up</button>";
+        $body .= "</form>";
+        $body .= "</body>";
+
+        return $body;
+    }
+}
--- src/app/Handler/LogoutHandler.php
+++ src/app/Handler/LogoutHandler.php
@@ -1,11 +1,17 @@
 <?php
+declare(strict_types=1);
 
-namespace App;
+namespace App\Handler;
 
-class LogoutHandler
+session_start();
+
+class LogoutHandler implements HandlerInterface
 {
-
-    public function __construct()
+    public function run(array $req): array
     {
+        $_SESSION = [];
+        session_destroy();
+        header("Location:http://localhost/posts",true, 301);
+        exit();
     }
 }
