83ec191172770c0d4c3b0aecbfadeeb819e4111c
toshiki-miyagawa <toshiki.miyagawa@prtimes.co.jp>
2023-06-26 10:32:35 +0900

    トランザクション処理の実装

--- src/app/Repository/PostUserRegisterRepository.php
+++ src/app/Repository/PostUserRegisterRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Service\PostUserRegisterService;
 use PDO;
 use App\Repository\DbConnect;
+use PDOException;
 
 class PostUserRegisterRepository
 {
@@ -20,22 +21,33 @@ class PostUserRegisterRepository
      */
     public static function insertUser($user, $pdo = null)
     {
-        if ($pdo === null) {
-            $pdo = DbConnect::dbConnect();
+
+        try {
+            if ($pdo === null) {
+                $pdo = DbConnect::dbConnect();
+            }
+
+            $pdo -> beginTransaction();
+
+            $insert_user = $pdo->prepare('INSERT INTO users (name,password,mail_address,profile_image_id) VALUES (:username, :email, :password,null) RETURNING user_id');
+            $params = array(':username' => $user->username, ':email' => $user->email, ':password' => $user->password);
+            $insert_user->execute($params);
+            $user_last_id = $insert_user->fetch(PDO::FETCH_ASSOC)['user_id'];
+
+            if (isset($user->profile_img_name)) {
+                $update_image_id = $pdo->prepare('UPDATE users SET profile_image_id=:last_id WHERE user_id =:last_id');
+                $params = array(':last_id' => $user_last_id);
+                $update_image_id->execute($params);
+            }
+
+            if ($insert_user && $update_image_id) {
+                $pdo->commit();
+                return $user_last_id;
+            }
+        } catch(PDOException $e) {
+            $pdo->rollBack();
+        } finally {
+            $pdo = null;
         }
-
-
-        $insert_user = $pdo->prepare('INSERT INTO users (name,password,mail_address,profile_image_id) VALUES (:username, :email, :password,null) RETURNING user_id');
-        $params = array(':username' => $user->username, ':email' => $user->email, ':password' => $user->password);
-        $insert_user->execute($params);
-        $user_last_id = $insert_user->fetch(PDO::FETCH_ASSOC)['user_id'];
-
-        if (isset($user->profile_img_name)) {
-            $update_image_id = $pdo->prepare('UPDATE users SET profile_image_id=:last_id WHERE user_id =:last_id');
-            $params = array(':last_id' => $user_last_id);
-            $update_image_id->execute($params);
-        }
-
-        return $user_last_id;
     }
 }
